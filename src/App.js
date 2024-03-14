80fb388d9bf8272cb797e9ad58ee4d4239079f01
Alles <alleskaput43@gmail.com>
2024-03-14 10:31:28 +0300

    Создан компонент модального окна (простенький)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Modal from "./components/Modal/Modal.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isVisible, setIsVisible] = useState(false);
+
+    function onCancel() {
+        setIsVisible(false);
+    }
+
+    function onSuccess() {
+        alert("Ok");
+        setIsVisible(false);
+    }
+
+    return (
+        <div className="App">
+            <button
+                onClick={() => {
+                    setIsVisible(true);
+                }}
+            >
+                Click me
+            </button>
+
+            <Modal
+                isVisible={isVisible}
+                setIsVisible={setIsVisible}
+                modalTitle="Header"
+                modalText="text text text text text text text text text text text text text text text text"
+                imageSource="https://co.radio.net/images/broadcasts/fb/5a/16085/1/c300.png"
+                onCancel={onCancel}
+                onSuccess={onSuccess}
+                isCloseIcon={true}
+                isFooter={true}
+                isCancelButton={true}
+                isImage={true}
+                onSuccessButtonLabel="Success"
+            ></Modal>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,64 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import classes from "./Modal.module.css";
+
+export default function Modal({
+    isVisible,
+    setIsVisible,
+    modalTitle,
+    modalText,
+    onCancel,
+    onSuccess,
+    isCloseIcon,
+    isFooter,
+    isCancelButton,
+    isImage,
+    onSuccessButtonLabel,
+    imageSource,
+}) {
+    if (!isVisible) return null;
+
+    return (
+        <>
+            <div className={classes.overlay}>
+                <div className={classes.modal}>
+                    {isImage && (
+                        <img
+                            className={classes.img}
+                            src={imageSource}
+                        />
+                    )}
+                    <div className={classes.modalContent}>
+                        <div className={classes.modalHeader}>
+                            <p className={classes.header}>{modalTitle}</p>
+                            {isCloseIcon && (
+                                <button onClick={() => setIsVisible(false)}>
+                                    <FontAwesomeIcon icon={faXmark} />
+                                </button>
+                            )}
+                        </div>
+
+                        <div className={classes.modalBody}>{modalText}</div>
+
+                        {isFooter && (
+                            <div className={classes.modalFooter}>
+                                <button onClick={() => onSuccess()}>
+                                    {onSuccessButtonLabel || "Done"}
+                                </button>
+
+                                {isCancelButton && (
+                                    <button
+                                        className={classes.okButton}
+                                        onClick={() => onCancel()}
+                                    >
+                                        Cancel
+                                    </button>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
